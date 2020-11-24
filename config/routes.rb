025fb2f399157f53d6834bd34cd6e3789f0acd77
Amanda Emmanuel <amandaemmanuel@Amandas-Air-2.phub.net.cable.rogers.com>
2020-11-24 16:07:07 -0500

    Upload images to recipes, styling homepage and recipe pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :recipes
+
+  #set recipes controller's index action to the root of the website
+  root "recipes#index"
 end
