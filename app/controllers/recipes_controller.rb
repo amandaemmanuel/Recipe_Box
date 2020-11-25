8c480abc8a5624cb1ba61a67365e58a0da1f10d6
Amanda Emmanuel <amandaemmanuel@Amandas-Air-2.phub.net.cable.rogers.com>
2020-11-25 18:27:57 -0500

    Fixed javascript with Rails 6 issue & added nested forms for ingredients and directions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,8 @@ class RecipesController < ApplicationController
 
     def new
         @recipe = Recipe.new
+        # @recipe_ingredient = @recipe.ingredients.build
+        # @ingredients = Ingredient.all
     end
 
     def create
@@ -39,10 +41,10 @@ class RecipesController < ApplicationController
     private
 
     def recipe_params
-        params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
-    end
+		params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
+	end
 
-    def find_recipe
-        @recipe = Recipe.find(params[:id]) 
-    end
+	def find_recipe
+		@recipe = Recipe.find(params[:id])
+	end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,8 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require cocoon
+require("jquery")
+require("cocoon")
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
